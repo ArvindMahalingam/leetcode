ee2ed96f8fed7e2e55d290dcd2201304c6853306
Arvind Mahalingam <arvind2410222@ssn.edu.in>
2026-09-05 16:11:56 +0530

    Time: 609 ms (56.19%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select distinct firstrow.num as ConsecutiveNums from Logs firstrow join Logs secondrow on firstrow.id+1=secondrow.id join Logs thirdrow on secondrow.id+1=thirdrow.id where firstrow.num=secondrow.num and secondrow.num=thirdrow.num;
+select  distinct firstrow.num as ConsecutiveNums from Logs firstrow join Logs secondrow on firstrow.id+1=secondrow.id join Logs thirdrow on secondrow.id+1=thirdrow.id where firstrow.num=secondrow.num
+and secondrow.num=thirdrow.num
