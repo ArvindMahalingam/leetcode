7b697a9fa8ce339123513bd712ca22d955bb4d03
Arvind Mahalingam <arvind2410222@ssn.edu.in>
2026-09-07 08:21:22 +0530

    Time: 1465 ms (9.08%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,6 +1,10 @@
-select distinct D.product_id,COALESCE(J.price,10) as price from  Products D left join
+select distinct X.product_id,COALESCE(J.PRICE,10) as price from Products X  left join
 (select P.product_id,P.new_price as price from Products P join
-(select product_id,max(change_date) as dd from Products where change_date<='2019-08-16' group by product_id)
-as date_match
-on P.product_id=date_match.product_id and P.change_date=date_match.dd) as J
-on J.product_id=D.product_id;
+(select product_id,max(change_date) as fd from Products where change_date<='2019-08-16' group by product_id) as ff
+on P.product_id=ff.product_id and ff.fd=P.change_date) as J
+
+on J.product_id=X.product_id
+
+
+
+
